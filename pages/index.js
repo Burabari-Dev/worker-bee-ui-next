40ea580b1f18586172aeb2983111145f645080ee
Burabari <burabaridev@gmail.com>
2022-04-15 05:31:53 +0100

    add @testing-library/user-event dependency

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import { useState } from 'react';
 import { Container } from 'semantic-ui-react'
+import AdminLogin from '../components/Admin-Login/Admin-Login';
 
 export default function Home() {
   return (
     <Container>
-      
+      <AdminLogin/>
     </Container>
   )
 }
