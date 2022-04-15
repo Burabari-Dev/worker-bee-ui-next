4c546e8c4a1639fb6c7d21ea8776e9c0974b45cf
Burabari <burabaridev@gmail.com>
2022-04-15 05:52:42 +0100

    create Admin-Login component

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
 import { useState } from 'react';
 import { Container } from 'semantic-ui-react'
-import AdminLogin from '../components/Admin-Login/Admin-Login';
 
 export default function Home() {
   return (
     <Container>
-      <AdminLogin/>
+      
     </Container>
   )
 }
